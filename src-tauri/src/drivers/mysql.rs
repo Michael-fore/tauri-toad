d354cb6e4fd2dd9a1d16cc18bbab718c61c966cf
Michael Watson-Fore <michaelfore2587@gmail.com>
2023-02-08 18:23:09 -0700

    working persistant connections

--- src-tauri/src/drivers/mysql.rs
+++ src-tauri/src/drivers/mysql.rs
@@ -35,12 +35,51 @@ impl MySQL{
 
     }
 
-    fn execute(&self, sql: &str) -> MyReturn{
-        let conn = self.get_conn();
+    fn execute(&self, sql: &str) -> MyReturn {
+        let conn = match self.get_conn(){
+            Ok(conn)=>conn,
+            Err(err)=> return MyReturn{
+                columns:vec![MyColumn{value:e.to_string(),datatype:"ERROR".to_string()}],
+                rows:vec![]
+            }
+        };
+
         let vec = match conn.query(sql){
             Ok(x)=>x.to_owned().into_iter().collect(),
-            Err(e)=> return Err(e)
+            Err(e)=>  return MyReturn{
+                columns:vec![MyColumn{value:e.to_string(),datatype:"ERROR".to_string()}],
+                rows:vec![]
+            }
         };
+
+        if vec.is_empty(){
+            return MyReturn{columns:vec![],rows:vec![]};
+        }
+
+        let mut out_columns = Vec::new();
+        for i in vec[0].columns_ref().clone(){
+            // let columns = i.; 
+            out_columns.push(MyColumn{value:i.name_str().to_string(), datatype:"".to_string()})
+        }
+    
+        for i in vec {
+    
+            //as of now every row loops through and filles the columns
+            // as a side project i am gonna leave it, but will proly 
+            //chagne to {key:value} output style
+    
+            let vals = i.unwrap();
+           
+            let mut vals_vec = Vec::new();
+            for val in vals{   
+                vals_vec.push(val.as_sql(true).to_owned())
+                // r.push(val.as_sql(true).to_owned());
+            }
+            r.push(MyRow{values:vals_vec});
+        }
+        let output = MyReturn{columns:out_columns, rows:r}; 
+        return output;
+
     }
 
     fn describe(){ 
